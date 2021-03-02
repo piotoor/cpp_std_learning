ad59b60a927e19dd261d13220e28a4e98a856be7
Piotr Kozka <piotoor@gmail.com>
2021-03-02 22:52:46 +0100

    Exit method can now be chosen via simple menu

--- cstdlib_process_control.cpp
+++ cstdlib_process_control.cpp
@@ -40,16 +40,33 @@ int main() {
     at_quick_exit(quick_foo);
     at_quick_exit(quick_bar);
 
-#ifdef EXIT
-    exit(0);          // destroys only static + runs atexit routines + exits normally
-#elseif QUICK_EXIT
-    quick_exit(0);    // doesn't destroy anything + runs at_quick_exit routines + exits normally
-#elseif _EXIT
-    _Exit(0);         // doesn't destroy anything + doesn't run anything + exits normally
-#elseif ABORT
-    abort();          // doesn't destroy anything + doesn't run anything + exits abnormally
-#else
-    return 0;         // destroys everything + runs atexit routines + exits normally
-#endif
-
+    std::cout << std::endl << "---------------------------------------------" << std::endl;
+    std::cout << "How do you want to end this?" << std::endl
+              << "e - exit()" << std::endl
+              << "q - quick_exit()" << std::endl
+              << "x - _Exit()" << std::endl
+              << "a - abort()" << std::endl
+              << "other - return" << std::endl;
+    char choice;
+    std::cout << "Your choice: ";
+    std::cin >> choice;
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << std::endl;
+    switch (choice) {
+      case 'e':
+        exit(0);        // destroys only static + runs atexit routines + exits normally
+        break;
+      case 'q':
+        quick_exit(0);  // doesn't destroy anything + runs at_quick_exit routines + exits normally
+        break;
+      case 'x':
+        _Exit(0);       // doesn't destroy anything + doesn't run anything + exits normally
+        break;
+      case 'a':
+        abort();        // doesn't destroy anything + doesn't run anything + exits abnormally
+        break;
+      default:
+        return 0;       // destroys everything + runs atexit routines + exits normally
+        break;
+    }
 }
